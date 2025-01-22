d14f5ffaa57a54109165677994e70e7ad9a090f2
EliasNeerbye <eliasnee@afk.no>
2025-01-22 11:43:41 +0100

    Connected to mongodb

--- RUST-API/userspace/src/main.rs
+++ RUST-API/userspace/src/main.rs
@@ -1,3 +1,7 @@
+use mongodb::Client;
+use mongodb::options::ClientOptions;
+use mongodb::bson::doc;
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -5,7 +9,24 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+async fn connect() -> Result<Client, mongodb::error::Error> {
+    let uri = "mongodb://localhost:27017/user-space-api";  // Corrected port
+    let options = ClientOptions::parse(uri).await?;
+    let client = Client::with_options(options)?;
+    
+    client.database("admin").run_command(doc! {"ping": 1}).await?;
+    println!("Successfully connected to MongoDB!");
+    Ok(client)
+}
+
+
 #[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+async fn rocket() -> _ {
+    let client = match connect().await {
+        Ok(client) => client,
+        Err(_) => panic!("Could not connect to MongoDB"),
+    };
+    rocket::build()
+        .manage(client)  // Store client in Rocket's managed state
+        .mount("/", routes![index])
 }
